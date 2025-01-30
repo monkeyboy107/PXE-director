8d21c0daf35f4f1204946809d2369b95b65980e3
Isaac Kerley <isaackerley@gmail.com>
2025-01-29 21:14:29 -0700

    Getting a hello world

--- web/app.py
+++ web/app.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+  return 'Hello world!'
+
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=5000, debug=True)
