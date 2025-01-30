dc8e12859412246a3cb53dd21596bd214ea6d1be
Isaac Kerley <isaackerley@gmail.com>
2025-01-29 23:22:17 -0700

    Updating sessions

--- web/app.py
+++ web/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 from flask_session import Session
 
 app = Flask(__name__)
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
+
+Session(app)
 
 @app.route('/')
 def index():
@@ -12,9 +14,16 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   if request.method == 'POST':
-    return "You're signed in"
+    session['name'] = request.form.get('username')
+    print(session)
+    return render_template('values.html.j2', value=session['name'])
   else:
-    return render_template('login.html.j2')
+    return render_template('login.html.j2', title='Please sign in')
+
+@app.route('/logout')
+def logout():
+  session['name'] = None
+  return redirect('/')
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=5000, debug=True)
